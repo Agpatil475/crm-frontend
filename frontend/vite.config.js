c70dafc0d9f6c479a25896c915aabaa3cbfdce0f
Agpatil475 <akshaygpatil0811@gmail.com>
2025-01-29 12:25:58 +0530

    frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "react-pro-sidebar/dist/styles.css":
+        "node_modules/react-pro-sidebar/dist/styles.css",
+      "@mui/material/styles": "@mui/material/esm/styles",
+    },
+  },
+});
